37242a1152f546d8e5d43efe327c12982b8c2181
Rayablepy <rayhan11.0808@gmail.com>
2026-09-22 13:19:14 +0800

    added model loading and testing scripts

--- loader.py
+++ loader.py
@@ -1,14 +1,23 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-load_choice=int(input("""
-    1.Local download
-    2.Huggingface download
-    """))
-if load_choice==1:
-    model = AutoModelForSequenceClassification.from_pretrained("./models/IMDBert")
-    tokenizer = AutoTokenizer.from_pretrained("./models/IMDBert")
-elif load_choice==2:
-    model = AutoModelForSequenceClassification.from_pretrained("Rayhan-08/IMDBert")
-    tokenizer = AutoTokenizer.from_pretrained("Rayhan-08/IMDBert")
-else:
-    print("Invalid option")
+def get_model():
+    load_choice=int(input("""
+        1.Local download
+        2.Huggingface download
+        """))
+    if load_choice==1:
+        try:
+            model = AutoModelForSequenceClassification.from_pretrained("./models/IMDBert")
+            tokenizer = AutoTokenizer.from_pretrained("./models/IMDBert")
+            return model,tokenizer
+        except Exception as e:
+            print(f"Error occurred while loading model: \n {e}")
+            return None
+    elif load_choice==2:
+        model = AutoModelForSequenceClassification.from_pretrained("Rayhan-08/IMDBert")
+        tokenizer = AutoTokenizer.from_pretrained("Rayhan-08/IMDBert")
+        return model,tokenizer
+    else:
+        print("Invalid error, defaulting to HuggingFace, ctrl+C to abort")
+        model = AutoModelForSequenceClassification.from_pretrained("Rayhan-08/IMDBert")
+        tokenizer = AutoTokenizer.from_pretrained("Rayhan-08/IMDBert")
+        return model,tokenizer
---
+++ tester.py
@@ -0,0 +1,42 @@
+from loader import get_model
+import torch.nn.functional as F
+import torch
+def get_device():
+    devtype=input("M for mac, W for windows: ").upper()
+    if devtype == "M":
+      device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    elif devtype=="W":
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+      device="cpu"
+    print(f"Using device: {device}")
+    return device
+def predict_sentiment(text, model, tokenizer, device):
+    model.eval()
+
+    inputs = tokenizer(
+        text,
+        truncation=True,
+        padding=True,
+        max_length=256,
+        return_tensors="pt",
+    ).to(device)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        probs = F.softmax(outputs.logits, dim=1)
+        pred = torch.argmax(probs, dim=1).item()
+        confidence = probs[0][pred].item()
+
+    label = "positive" if pred == 1 else "negative"
+    return label, confidence
+
+device=get_device()
+model,tokenizer=get_model()
+model.to(device)
+while True:
+    review = input("Review(q to quit): \n")
+    label, confidence = predict_sentiment(review, model, tokenizer, device)
+    print(f"Prediction: {label} ({confidence:.2%} confidence)")
+    if review=="q":
+        break
